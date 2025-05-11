da7c5bb31d6693bb4daf1a20920ace9edbc4ec30
FEFO2 <francescfouine@gmail.com>
2025-05-11 17:45:51 +0200

    finished

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import random
 
 def excuse_generator():
+    excuse = input("What's the reason you need an excuse today? ")
     who = ["The dog","The cat","My sister","My neighbour","The japanese mafia"]
+    what = ["burnt", "lost", "took away", "dissapeared", "pissed"]
+    when = ["just 5 minutes ago", "yesterday", "this morning", "last sunday", "last night"]
+    print(random.choice(who) + " " +  random.choice(what) + " " + excuse + " " + random.choice(when))
+
+excuse_generator()
